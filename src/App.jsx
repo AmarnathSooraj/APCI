ce2e9c65d542b37403d2b3d09b26cf5619c3b450
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2025-03-18 21:34:24 +0530

    ffrgv

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/Home'
-import Footer from './Components/Footer/Footer'
-function App() {
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
+
+function AppContent() {
+  const location = useLocation();
+
+  if (location.pathname !== "/") {
+    return (
+      <div className="coming-soon">
+        <h1>Coming Soon</h1>
+      </div>
+    );
+  }
 
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<AppContent />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import navlogo from '../../assets/apcilogo.jpg'
-import menu from '../../assets/icons8-menu-bar-50.png'
-import './Navbar.css'
-function Navbar() {
+import React from 'react';
+import ktu from '../../assets/ktu.png';
+import vda from '../../assets/vda.jpeg';
+import menu from '../../assets/icons8-menu-bar-50.png';
+import './Navbar.css';
 
+function Navbar() {
   return (
-    <div className='navbar'>
+    <header className="navbar">
       <div className="nav1">
-        <p>Mon-Thu<span> 8:00 am to 9:00pm</span></p>
-        <div className='spc'>
+        <p>Mon-Thu<span> 8:00 am to 9:00 pm</span></p>
+        <div className="spc">
           <a href="#">Support |</a>
           <a href="#"> Privacy |</a>
           <a href="#"> Contact</a>
         </div>
       </div>
-      <img className='nav2' src={navlogo}/>
-      <div className='nav3'>
-        <div className='menu1'>
-        <a href='#'>Home</a>
-        <a href='#'>About</a>
-        <a href='#'>Authors</a>
-        <a href='#'>Committee</a>
-        <a href='#'>Speakers</a>
-        <a href='#'>Event/Details</a>
-        <a href='#'>Registration</a>
-        <a href='#'>Venue/Travel</a>
-        <a href='#'>Contact Us</a>
-        </div>
-        {/* <img src={menu} className='menu2' /> */}
+      <div className="nav2">
+        <img src={vda} alt="College Logo 1" />
+        {/* <img src={ktu} alt="College Logo 2" /> */}
+        <h1>InRacs2025</h1>
+        <img src={ktu} alt="College Logo 3" />
       </div>
-    </div>
-  )
+      <nav className="nav3">
+        <div className="menu1">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/committee">Committee</a>
+          <a href="/speakers">Speakers</a>
+          <a href="/event-details">Event/Details</a>
+          <a href="/registration">Registration</a>
+          <a href="/contact-us">Contact Us</a>
+        </div>
+        {/* <img src={menu} className="menu2" alt="Menu" /> */}
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
