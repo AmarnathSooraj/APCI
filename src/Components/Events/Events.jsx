bad6a9d6acdb96d4ffd6d04dffd66738519e477a
Abhin <abhinchelakkal@gmail.com>
2025-05-23 23:52:57 +0530

    values

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -6,7 +6,7 @@ function Events() {
     <div className="event-container">
       <h2>Event Details</h2>
       <div className="event-card">
-        <p><strong>Last date for paper submission:</strong> 20th May 2025</p>
+        <p><strong>Last date for paper submission:</strong> 30th May 2025</p>
         <p><strong>Acceptance notification:</strong> 05th June 2025</p>
         <p><strong>Last date for camera-ready copy submission:</strong> 15th May 2025</p>
       </div>
